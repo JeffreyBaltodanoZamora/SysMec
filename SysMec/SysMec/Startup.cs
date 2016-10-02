5bf1ab5b556cfd48d50a8eb3b35fe80f18f8f8b4
Lenovo <Lenovo@-JEFFREY>
2016-10-02 17:55:26 -0600

    Creacion del proyecto

---
+++ SysMec/SysMec/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SysMec.Startup))]
+namespace SysMec
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
