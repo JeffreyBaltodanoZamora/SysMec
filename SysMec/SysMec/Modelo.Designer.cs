a3399ae061985f8d53b0139c48b39340475ba58d
Enmanuel94 <emach234@gmail.com>
2016-10-25 20:57:15 -0600

    se quito el spanglish

--- SysMec/SysMec/Modelo.Designer.cs
+++ SysMec/SysMec/Modelo.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Sonia\Source\Repos\SysMec3\SysMec\SysMec\Modelo.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\emanuel\Source\Repos\SysMec\SysMec\SysMec\Modelo.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
